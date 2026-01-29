07ded29d7ddc5f612482b3a9596302d109137a85
SudoNut <2023683472@student.uitm.edu.my>
2026-01-29 10:54:22 +0000

    1234

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$host = "127.0.0.1";
-$username = "dbuser";
-$password = "dbuser@123";
+$host = "localhost";
+$username = "root";
+$password = "";
 $database = "membership_system";
 
 $conn = new mysqli($host, $username, $password, $database);
